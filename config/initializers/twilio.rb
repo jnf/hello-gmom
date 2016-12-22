6b9e2db801893d4a235ee07c60151502fb5a1148
jnf <jeremy.flores@gmail.com>
2016-12-22 07:37:50 -0800

    make a values object with default twiml responses

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -2,3 +2,8 @@ Twilio.configure do |config|
   config.account_sid = ENV["TWILIO_ACCOUNT_SID"]
   config.auth_token = ENV["TWILIO_AUTH_TOKEN"]
 end
+
+TWIML = Value.new(:unauthorized, :thanks).with({
+  unauthorized: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>204 Unauthorized</Message></Response>",
+  thanks: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>Ok! Your message is being processed!</Message></Response>"
+})
