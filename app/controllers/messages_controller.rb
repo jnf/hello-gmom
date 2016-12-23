825d5bd90e5099b158c9960989e81b5b34da2595
jnf <jeremy.flores@gmail.com>
2016-12-22 20:07:01 -0800

    do some meaningful error handling in the message dispatch action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,9 +5,13 @@ class MessagesController < ApplicationController
 
   def incoming
     sender = Sender.from_number(params.fetch("From", nil))
-    render xml: TWIML.unauthorized unless sender
+    raise Twilio::UnauthorizedError unless sender
 
     Message.create MmsDecoder.new(sender, params).to_h
     render xml: TWIML.thanks
+  rescue Twilio::UnauthorizedError
+    render xml: TWIML.unauthorized
+  rescue Exception
+    render xml: TWIML.error
   end
 end
--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -3,7 +3,14 @@ Twilio.configure do |config|
   config.auth_token = ENV["TWILIO_AUTH_TOKEN"]
 end
 
-TWIML = Value.new(:unauthorized, :thanks).with({
-  unauthorized: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>204 Unauthorized</Message></Response>",
-  thanks: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>Ok! Your message is being processed!</Message></Response>"
+TWIML = Value.new(:unauthorized, :thanks, :error).with({
+  unauthorized: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>403 Forbidden</Message><Message>I don't know you.</Message></Response>",
+  thanks: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>200 Ok!</Message><Message>G'mom will see your message soon.</Message></Response>",
+  error: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>500 Error. Something went wrong, but it's not your fault. Try again in a few minutes.</Message></Response>"
 })
+
+class Twilio::UnauthorizedError < StandardError
+  def initialize(msg = TWIML.unauthorized)
+    super
+  end
+end
