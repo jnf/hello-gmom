96452ea65f6dd01d846845db43a2ca818de8f82d
jnf <jeremy.flores@gmail.com>
2016-12-22 22:08:37 -0800

    overload the paperclip path so we can render the avatar in a view

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -3,6 +3,7 @@ class Sender < ApplicationRecord
 
   has_attached_file :avatar,
     path: ':rails_root/public/system/:class/:filename',
+    url: '/system/:class/:filename',
     default_url: "/images/:style/missing.png"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
