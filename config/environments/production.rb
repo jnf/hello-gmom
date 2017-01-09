106b0d3bb60f74ee1284a5abb7671ffd63b2b27d
jnf <jeremy.flores@gmail.com>
2017-01-09 14:29:14 -0800

    explicitely allows https connections in action cable production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,12 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://hello-gmom.com', 'http://159.203.232.164' ]
+  config.action_cable.allowed_request_origins = [
+    'http://hello-gmom.com',
+    'http://159.203.232.164',
+    'https://hello-gmom.com',
+    'https://159.203.232.164'
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
