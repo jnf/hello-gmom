7bd76c9161fea00ccb9ebc3ffcbf2ce03be36b58
jnf <jeremy.flores@gmail.com>
2017-06-20 14:36:07 -0700

    adds empty bodies in the message excludes for presentation purposes

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,7 +12,7 @@ class Message < ApplicationRecord
 
   before_save :define_orientation
 
-  scope :has_body, -> { where.not(body: nil) }
+  scope :has_body, -> { where.not(body: [nil, '']) }
 
   def self.for_gmom
     self.includes(:sender).has_body.order(created_at: :desc).limit(50)
