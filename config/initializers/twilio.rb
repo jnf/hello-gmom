d2e778b2c3854836aec376c08454fd87edbb354d
Jeremy Flores <jeremy.flores@smashingideas.com>
2017-08-04 14:23:10 -0700

    adds another twiml message to use when sending feedback to alexa skill

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -3,10 +3,11 @@ Twilio.configure do |config|
   config.auth_token = ENV["TWILIO_AUTH_TOKEN"]
 end
 
-TWIML = Value.new(:unauthorized, :thanks, :error).with({
+TWIML = Value.new(:unauthorized, :thanks, :error, :redirect).with({
   unauthorized: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>403 Forbidden</Message><Message>I don't know you.</Message></Response>",
   thanks: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>200 Ok!</Message><Message>G'mom will see your message soon.</Message></Response>",
-  error: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>500 Error. Something went wrong, but it's not your fault. Try again in a few minutes.</Message></Response>"
+  error: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>500 Error. Something went wrong, but it's not your fault. Try again in a few minutes.</Message></Response>",
+  redirect: "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><Response><Message>200 OK</Message><Message>Clients Redirecting</Message></Response>"
 })
 
 class Twilio::UnauthorizedError < StandardError
